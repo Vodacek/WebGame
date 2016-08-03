e169c8c4bb423bb3cdb0ce705a1d4a7992331c15
satikcz <lubos.behounek@gmail.com>
2016-08-03 08:30:33 +0200

    Initial commit

---
+++ Game/Cube3D/Game/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube3D.Game
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+
+        public Player(string playerName)
+        {
+            this.Name = playerName;
+        }
+    }
+}
---
+++ Game/Cube3D/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube3D
+{
+    public class Test
+    {
+        public int x;
+            public int y;
+    }
+}
