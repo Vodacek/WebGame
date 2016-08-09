221e42f4a38779293b52dab7ca2a8fac964e30f4
satikcz <lubos.behounek@gmail.com>
2016-08-09 19:00:13 +0200

    +rectanglef and animationtype

---
+++ Game/Cube3D/Helpers/RectangleF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game3D.Helpers
+{
+    public struct RectangleF
+    {
+        Vector2 TopLeft { get; set; }
+        Vector2 BottomRight { get; set; }
+    }
+}
---
+++ Game/Cube3D/Rendering/AnimationType.cs
@@ -0,0 +1,35 @@
+﻿using Bridge.WebGL;
+using Game3D.Helpers;
+using System.Collections.Generic;
+
+namespace Game3D.Rendering
+{
+    public class AnimationType
+    {
+        public WebGLTexture Texture { get; set; }
+        public float Speed { get; set; }
+        public Vector2 TexSize { get; set; }
+        public Dictionary<string, anim >
+
+        public Animation(WebGLTexture texture, Vector2 texSize)
+        {
+            this.Texture = texture;
+            this.TexSize = texSize;
+        }
+
+        public void AddAnimation(string name, int count, )
+        {
+
+        }
+
+        public RectangleF GetTextureCoords()
+        {
+            return 
+        }
+
+        public void ChangeAnimation(string name)
+        {
+
+        }
+    }
+}
