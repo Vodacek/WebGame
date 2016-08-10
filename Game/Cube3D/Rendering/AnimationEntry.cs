dfdded3627f35cbaa0a2b213959050aa15983035
satikcz <lubos.behounek@gmail.com>
2016-08-10 08:47:34 +0200

    started working on animation

---
+++ Game/Cube3D/Rendering/AnimationEntry.cs
@@ -0,0 +1,28 @@
+﻿using Game3D.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game3D.Rendering
+{
+    public class AnimationEntry
+    {
+        public AnimationEntry(Vector2 firstFrame, Vector2 frameSize, int animationCount, float AnimationSpeed=1.0f)
+        {
+            this.FirstFrame = firstFrame;
+            this.FrameSize = frameSize;
+            this.AnimationCount = animationCount;
+            this.AnimationSpeed = AnimationSpeed;
+        }
+
+        public int AnimationCount { get; set; }
+
+        public Vector2 FirstFrame { get; set; }
+
+        public Vector2 FrameSize { get; set; }
+
+        public float AnimationSpeed { get; set; }
+    }
+}
--- Game/Cube3D/Rendering/AnimationType.cs
+++ Game/Cube3D/Rendering/AnimationType.cs
@@ -7,29 +7,23 @@ namespace Game3D.Rendering
     public class AnimationType
     {
         public WebGLTexture Texture { get; set; }
-        public float Speed { get; set; }
         public Vector2 TexSize { get; set; }
-        public Dictionary<string, anim >
+        public Dictionary<string, AnimationEntry> Animations { get; set; }
 
-        public Animation(WebGLTexture texture, Vector2 texSize)
+        public AnimationType(WebGLTexture texture, Vector2 texSize)
         {
             this.Texture = texture;
             this.TexSize = texSize;
         }
 
-        public void AddAnimation(string name, int count, )
+        public void AddAnimation(string name, Vector2 firstFrame, Vector2 frameSize, int frameCount, float speed = 1.0f)
         {
-
+            Animations.Add(name, new AnimationEntry(firstFrame, frameSize, frameCount, speed));
         }
 
-        public RectangleF GetTextureCoords()
+        public RectangleF GetFrameCoords(string animName)
         {
-            return 
-        }
-
-        public void ChangeAnimation(string name)
-        {
-
+            return Animations[animName].GetFrameCoords();
         }
     }
 }
