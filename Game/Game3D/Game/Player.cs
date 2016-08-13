f25b895898f9a99f10ed9d91beabcd402efed64f
satikcz <lubos.behounek@gmail.com>
2016-08-13 19:41:35 +0200

    player / npc

--- Game/Game3D/Game/Player.cs
+++ Game/Game3D/Game/Player.cs
@@ -11,9 +11,59 @@ namespace Game3D.Game
         public string Name { get; private set; }
         public Inventory Inventory { get; }
 
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Inteligence { get; set; }
+        public int Vitality { get; set; }
+
+        public float HitPoints { get; set; }
+        public float MaxHitPoints { get; set; }
+
+        public float Mana { get; set; }
+        public float MaxMana { get; set; }
+
+        public int Level { get; set; }
+        public int Experience { get; set; }
+
+        public int Armor { get; set; }
+        public float Resistance { get; set; }
+
         public Player(string playerName)
         {
             this.Name = playerName;
+            this.Level = 0;
+            this.Experience = 0;
+            this.Armor = 0;
+            this.Resistance = 0;
+            this.Dexterity = 10;
+            this.Strength = 10;
+            this.Inteligence = 10;
+            this.Vitality = 10;
+            this.MaxHitPoints = 10;
+            this.HitPoints = this.MaxHitPoints;
+            this.MaxMana = 10;
+            this.Mana = this.MaxMana;
+            this.Inventory = new Inventory();
+        }
+
+        /// <summary>
+        /// This is called when player is attacked by anything
+        /// </summary>
+        /// <param name="normalDamage">Normal damage type value</param>
+        /// <param name="magicDamage">Magic damage type value</param>
+        public void OnAttacked(float normalDamage, float magicDamage)
+        {
+            this.HitPoints -= normalDamage - Armor;
+            this.HitPoints -= magicDamage * (1 - Resistance);
+        }
+
+        /// <summary>
+        /// This is called when player kills anything
+        /// </summary>
+        public void OnKillActor(ActorGameObject actor)
+        {
+            this.Experience += actor.Experience;
+            this.Inventory.PickupWholeInventory(actor.Inventory);
         }
     }
 }
